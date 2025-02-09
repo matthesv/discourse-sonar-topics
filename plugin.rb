990296daf62b6c7ba3e2bf872d15212a7ec8585f
Matthes Vogel <matthesv@gmail.com>
2025-02-09 14:51:45 +0100

    Refactor SonarApiClient implementation and update file paths for better organization

--- plugin.rb
+++ plugin.rb
@@ -2,14 +2,14 @@
 # about: Erzeugt automatisch Themen mithilfe der Sonar Perplexity API.
 # version: 0.2.0
 # authors: Matthes
-# url: https://github.com/matthesv/discourse-sonar-topics
+# url: https://github.com/deinusername/discourse-sonar-topics
 
 enabled_site_setting :sonar_topics_enabled
 
 after_initialize do
-  # Unsere Service-Klasse laden
-  require_dependency File.expand_path("../lib/discourse-sonar-topics/sonar_api_client.rb", __dir__)
+  # Lade die Service-Klasse aus dem lib-Verzeichnis (relativer Pfad korrekt gesetzt)
+  require_dependency File.expand_path("lib/discourse-sonar-topics/sonar_api_client.rb", __dir__)
 
-  # Lade Jobs und weitere Klassen
-  load File.expand_path("../jobs/scheduled/sonar_topic_creation_job.rb", __FILE__)
+  # Lade den Scheduled Job (relativer Pfad korrekt gesetzt)
+  load File.expand_path("jobs/scheduled/sonar_topic_creation_job.rb", __dir__)
 end
